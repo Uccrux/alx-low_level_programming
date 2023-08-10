b392a116fedf1e70b7679dc57e69b7349ad9bb90
Uccrux <uccrux@gmail.com>
2023-08-10 14:08:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
 *main-Entry point
 *
-*Return:Always 0 (Succes)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like bulding a multilingual puzzle");
 
 return (0);
 }
