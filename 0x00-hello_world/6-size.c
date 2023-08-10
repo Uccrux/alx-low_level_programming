291be5966031846ae009213b2dd6fa53d8c65bb6
Uccrux <uccrux@gmail.com>
2023-08-10 16:10:30 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main- main block
+*Return: 0
+*/
+int main(void)
+
+{
+
+printf("Size of char: %i byte(s)\n", sizeof(char));
+printf("Size of int: %i byte(s)\n", sizeof(int));
+printf("Size of long int: %i byte(s)\n", sizeof(long int));
+printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of float: %i bytes(s)\n" sizeof(float);
+return (0);
+
+}
