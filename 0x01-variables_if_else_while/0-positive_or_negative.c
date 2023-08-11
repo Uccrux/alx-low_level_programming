307aff4f6a298f15f9577bfea34301e111677649
Uccrux <uccrux@gamil.com>
2023-08-11 18:03:18 +0000

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - assign a radom number to int n
+ * executes and print it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rad() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+
+return (0);
+}
