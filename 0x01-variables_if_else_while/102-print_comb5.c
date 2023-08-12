96138b4e75e7045e60abb8448d83de46ba3faf9b
Uccrux <uccrux@gamil.com>
2023-08-12 20:06:47 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible combinations of two digit numbers.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y, z;
+
+	for (y = 0; y < 100; y++)
+	{
+		for (z = 0; z < 100; z++)
+		{
+			if (y > z)
+			{
+				putchar((y/10) + 48);
+				putchar((y % 10) +48);
+				putchar(' ');
+				putchar((z/10) +48);
+				putchar((z % 10) + 48);
+				if (y != 98 || z != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
