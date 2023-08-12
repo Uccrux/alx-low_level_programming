89cc7caa06df303f73b28ef2299d5d8bab738598
Uccrux <uccrux@gamil.com>
2023-08-12 11:13:55 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - print the alphabet in lower case , and the uppercase,
+ * followed by a new line
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n");
+return(0);
+}
