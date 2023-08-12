51cec143a66c276c533ad64f55c53a10d7b2a305
Uccrux <uccrux@gamil.com>
2023-08-12 18:07:14 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 		for (b = 49; b <= 58; b++)
 		{
-			for (c = 50; c <= 58 c++)
+			for (c = 50; c <= 58; c++)
 			{
 				if (c > b && b > a)
 				{
@@ -31,3 +31,4 @@ int main(void)
 		putchar('\n');
 		return (0);
 	}
+}
