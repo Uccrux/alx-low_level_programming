7c3414e06411d4f38af6b148daa9a955072076dd
Uccrux <uccrux@gamil.com>
2023-08-12 12:03:22 +0000

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar( '\n')
 
-		return(0)
+		return(0);
 }
