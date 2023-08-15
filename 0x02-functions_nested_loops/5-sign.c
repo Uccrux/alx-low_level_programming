727e4113e0b4e9ab5136d64b823cc86cc961e5cb
Uccrux <uccrux@gamil.com>
2023-08-15 21:18:49 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - sign of number
+ * @n: n is an integer
+ * Return: 1+ , 0 , -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(+);
+		return (1);
+	}
+	else
+	{
+		if (n == 0)
+		{
+			_putchar(0);
+			return (0);
+		}
+		else
+		{
+			if (n < 0)
+			{
+				putchar(-)
+					return (-1);
+			}
+		}
+	}
+}
