7241cc446c1b25b2c182d81b30a3a9963a8cc22a
Uccrux <uccrux@gamil.com>
2023-08-15 15:08:34 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	-putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
