16427bd16789bf71ed820b3de1cfbcb566f4b651
Uccrux <uccrux@gamil.com>
2023-08-15 16:12:36 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - prints alphabets in lowercase
- * followed by a new line
+ * main - prints alphabet in a lowercase
+ *
+ * Return: 0;
  */
-
-void print_alphabet(void);
-
+void print_alphabet(void)';
 {
-	int i;
+	{
+		int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-			_putchar('\n');
-
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	}
+	-putchar('\n')
 }
