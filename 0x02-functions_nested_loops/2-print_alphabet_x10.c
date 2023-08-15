fd5d028af0de147bf217e636867747d22595c657
Uccrux <uccrux@gamil.com>
2023-08-15 17:25:51 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (i = '1'; i <= '10' i++)
+	for (i = '1'; i <= '10'; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
