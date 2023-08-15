dfb096f18a6b65f52b4ed674684bfd86b4629aec
Uccrux <uccrux@gamil.com>
2023-08-15 20:26:21 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _isalpha - check lowercase and uppercase character
+ * @c: the character is ASCII code
+ * Return: 1 for lowercase and uppercase 0 for rest
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 123 || c >= 65 && <= 91)
+	{
+		return (1);
+		{
+			else
+			{
+				return (0);
+			}
+		}
+	}
