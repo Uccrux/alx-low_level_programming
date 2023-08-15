e64b4f48b65439e3e59fe98641743212a9153103
Uccrux <uccrux@gamil.com>
2023-08-15 15:44:25 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = a; i <= z; i++)
+		_printchar(i)
+	return (0);
+}
