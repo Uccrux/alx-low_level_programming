9650c87c5ab64900296a6ec01b9fae8e4e217b5b
Uccrux <uccrux@gamil.com>
2023-08-15 22:18:35 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n: integer
+ * Return: return absolute valuer of a number
+ */
+iny _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
