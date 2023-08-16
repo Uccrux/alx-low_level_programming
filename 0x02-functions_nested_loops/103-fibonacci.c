e9d5573dca6f788091efd4eb5c546ea21957e544
Uccrux <uccrux@gamil.com>
2023-08-16 08:24:04 +0000

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,8 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  * followed by a new line
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
@@ -10,7 +12,6 @@ int main(void)
 
 	j = 1;
 	k = 2;
-
 	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
