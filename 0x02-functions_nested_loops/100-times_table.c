3e59372aff1a4877d5dc524e8882591088aebed3
Uccrux <uccrux@gamil.com>
2023-08-16 07:36:51 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,27 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * print_times_table - prints time tables of n
+ * print_times_table - prints time table of n
  *
  * @n: takes number input
  */
+
 void print_times_table(int n)
 {
 	int prod, mult, num;
 
 	if (n <= 15 && n >= 0)
-
-		for (n = 0; num <= n; ++num)
+	{
+		for (num = 0; num <= n; ++num)
 		{
 			_putchar(48);
 			for (mult = 1; mult <= n; ++mult)
 			{
 				_putchar(',');
 				_putchar(' ');
-				prod =  num * mult;
+
+				prod = num * mult;
+
 				if (prod <= 9)
 					_putchar(' ');
 				if (prod <= 99)
@@ -27,11 +31,11 @@ void print_times_table(int n)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
-				}
-				else if (prod <= 99 && prod >= 10)
+				} else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
+	}
 }
