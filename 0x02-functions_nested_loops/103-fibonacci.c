c28c3a3f0e79465528be16d36786fc61f3ee1fde
Uccrux <uccrux@gamil.com>
2023-08-16 08:17:40 +0000

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - prints the sum of the even-valued terms
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  * followed by a new line
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
