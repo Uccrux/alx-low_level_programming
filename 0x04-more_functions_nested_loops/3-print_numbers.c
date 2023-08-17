dfb09f8cf566a70d92d005e00621a9daa4116a6b
Uccrux <uccrux@gamil.com>
2023-08-17 22:20:12 +0000

    nested loop 2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - write a function that prints the number, from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
