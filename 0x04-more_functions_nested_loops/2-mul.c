0b9aa262f7f0bd7667908f6ff4f880eec5977dd7
Uccrux <uccrux@gamil.com>
2023-08-17 16:22:58 +0000

    nested loop 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,10 @@
 #include<stdio>
 /**
 * mul - write a function that multiplies two integers
-* @a: integer a
-* @b: integer b
+* @a: first integer
+* @b: second integer
 * Return: addition of a and b
-**/
+*/
 int mul(int a, int b)
 {
 	return (a * b);
