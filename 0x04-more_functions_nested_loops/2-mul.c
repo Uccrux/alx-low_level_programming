df49a9f3d13a549f3dc60a68df40bca771c119f8
Uccrux <uccrux@gamil.com>
2023-08-17 16:26:02 +0000

    nested loop 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
+#include <stdio.h>
 #include "main.h"
-#include<stdio>
 /**
-* mul - write a function that multiplies two integers
+* mul - multiplies two integers
 * @a: first integer
 * @b: second integer
 * Return: addition of a and b
