00d109e4dfd4d3e8f303b3a18cb7889f0ca6a282
Uccrux <uccrux@gamil.com>
2023-08-18 13:48:47 +0000

    nested loop 2

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: integer params
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
