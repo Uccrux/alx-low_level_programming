85b16486b832369648ab19162ca75738daf0faa7
Uccrux <uccrux@gamil.com>
2023-08-21 11:36:04 +0000

    pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * rev_string -prints a reverse string
- *
- * @s: The string to reverse
+ * rev_string - reverse a string
+ * @s: string to reverse
  */
 void rev_string(char *s)
 {
