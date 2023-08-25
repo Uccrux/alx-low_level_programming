8249a2a8585f3fcf88d86497bcdbc6bb522f8c59
Uccrux <uccrux@gamil.com>
2023-08-25 08:35:36 +0000

    pointers and arrays

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,19 @@
-
 #include "main.h"
 
 /**
- * * reverse_array -> reversing an array
- * * @a: array a
- * * @n: an element of an array
+ * reverse_array - resverse array of integers
+ * @a: array
+ * @n: number of elements of array
  */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int i;
+	int temp;
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < i / 2; k++)
+	for (i = 0; i < n--; i++)
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }
