f0e5370d31de51ea1fccc884bfc7c1173c5f13b1
Uccrux <uccrux@gamil.com>
2023-08-30 15:34:25 +0000

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+			return;
+	}
+		_putchar(*s);
+		s++;
+			_puts_recursion(s);
+}
+
