bfffeb0b98b8fc95fbc8cabd0b059386f63b8098
Uccrux <uccrux@gamil.com>
2023-08-31 20:46:45 +0000

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,29 @@
 #include "main.h"
+
 /**
- * find - find the prime number of an integer
- * @a: integer 1
- * @b: integer 2
- *
- * Return: return 1 for primenunber or 0
+ * check - checks to see if number is prime
+ * @a:int
+ * @b:int
+ * Return:int
  */
-int find(int a, int b)
+int check(int a, int b)
 {
-	if (a <  2 || b % a == 0)
+	if (b < 2 || b % a == 0)
 		return (0);
 	else if (a > b / 2)
 		return (1);
-	{
-		return (find(a + 1, b));
-	}
+	else
+		return (check(a + 1, b));
 }
 
 /**
- * is_prime_number - The prime number of an integer
- * @n: integer
- *
- * Return: integer
+ * is_prime_number - states if number is prime
+ * @n:int
+ * Return:int
  */
 int is_prime_number(int n)
 {
 	if (n == 2)
 		return (1);
-	return (find(2, n));
+	return (check(2, n));
 }
