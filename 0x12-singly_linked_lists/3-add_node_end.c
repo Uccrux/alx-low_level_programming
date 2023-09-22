4681d1b5ef09ebb6b8d90baebea98a06814d2831
Uccrux <uccrux@gamil.com>
2023-09-22 13:03:40 +0000

    singly linked

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,47 +1,41 @@
 #include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a list
- * @head: pointer to the head of the list
- * @str: string to be added to the list
- *
- * Return: NULL in case of failure
- * or address or the element
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new, *last;
+	list_t *new_node, *temp;
+	size_t nchar;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (str == NULL)
+	new_node->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new_node->len = nchar;
+	new_node->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
 	{
-		free(new);
-		return (NULL);
+		*head = new_node;
 	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
 	else
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
 	}
+
 	return (*head);
 }
-Footer
