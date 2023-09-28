89a6ff9a900ec807c566063252065f7d6240610b
Uccrux <uccrux@gamil.com>
2023-09-28 16:54:26 +0000

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: number to set
+ * @index: index at which to set bit
  *
- * Return: Always 0.
+ * Return: 1 if it worked, or -1 if an error occurred
  */
-int main(void)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int n;
+	unsigned long int set;
 
-    n = 1024;
-    set_bit(&n, 5);
-    printf("%lu\n", n);
-    n = 0;
-    set_bit(&n, 10);
-    printf("%lu\n", n);
-    n = 98;
-    set_bit(&n, 0);
-    printf("%lu\n", n);
-    return (0);
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	set = 1 << index;
+	*n = *n | set;
+	return (1);
 }
