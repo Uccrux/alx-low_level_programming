a6a9e045707f3b13ba806516969f809db96f8d7b
Uccrux <uccrux@gamil.com>
2023-10-26 11:58:04 +0000

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,8 +17,10 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
+
 			return (0);
-		dec_val = ( dec_val * 2) + (b[i] - '0');
+
+		dec_val = (dec_val * 2) + (b[i] - '0');
 	}
 
 	return (dec_val);
